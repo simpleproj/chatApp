eaf6fb50fa7c763e27150c372bbffa9545373ae5
simpleproj <123255069+simpleproj@users.noreply.github.com>
2024-01-11 18:36:43 +0300

    Delete src/main/java/com/mysite/stc directory

--- src/main/java/com/mysite/stc/Main.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.stc;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Boris");
-        System.out.println("Really! It's very foo!");
-        System.out.println("This is super feature!");
-    }
-}
